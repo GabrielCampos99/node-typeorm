3867fee07e3987cc2a09326622baffee0b546ab2
Gabriel Campos <gabriel.campos@alicerceedu.com.br>
2021-12-30 16:14:20 -0300

    add manyToMany relations

--- src/entities/Bankers.ts
+++ src/entities/Bankers.ts
@@ -2,12 +2,13 @@
 
 import {
   Entity,
-  BaseEntity,
   Column,
-  PrimaryColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToMany,
+  JoinTable,
 } from "typeorm";
+import { Client } from "./Client";
 import { Person } from "./utils/Person";
 
 //Invocar a entitie e especificar no nome da tabela que queremos em letra minuscula
@@ -19,9 +20,32 @@ export class Banker extends Person {
   })
   employee_number: string;
 
+  @ManyToMany(() => Client)
+  @JoinTable({
+    name: "bankers_clients",
+    joinColumn: {
+      name: "banker",
+      referencedColumnName: "id",
+    },
+    inverseJoinColumn: {
+      name: "client",
+      referencedColumnName: "id",
+    },
+  })
+  clients: Client[];
+
   @CreateDateColumn()
   created_at: Date;
 
   @UpdateDateColumn()
   update_at: Date;
 }
+
+//UM BANCO PODE TER VARIOS CLIENTES E UM CLIENTE PODE TER VARIOS BANCOS; @ManyToMany
+
+//Quandos nos temos um manytomany, não não pedems ter apenas uma foreing key nessa entitie, nos precisamos criar uma tabela separada e a outra tabela conterá apenas duas colunas nesse caso, 1° id_banker, 2°id_client. e essas duas colunas irá linkar as duas entities juntas
+
+// client_id          banker_id
+//     1                  3
+//     1                  2
+//     3                  3
--- src/entities/Transaction.ts
+++ src/entities/Transaction.ts
@@ -1,4 +1,12 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Client } from "./Client";
 
 export enum TransactionType {
   DEPOSIT = "deposit",
@@ -20,4 +28,17 @@ export class Transaction extends BaseEntity {
     type: "numeric",
   })
   amount: number;
+
+  @ManyToOne(() => Client, (client) => client.transactions)
+  @JoinColumn({
+    name: "client_id",
+  })
+  client: Client;
 }
+
+//Um transação pode ser de apenas um cliente , mas o cliente pode ter VARIAS  transações
+//ManyToOne()
+//primeiro parametro retorna uma funcao
+//uma função que retorna Client
+////outro parametro é pegar todos os clientes e retornar o client.transactions
+//tem que fazer a mesma coisa lá na entitie Client
