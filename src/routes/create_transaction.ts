6c296d9bdeb227c1ac31c6fd78d2afc941a2112c
Gabriel Campos <gabriel.campos@alicerceedu.com.br>
2021-12-31 08:26:43 -0300

    add transactions

---
+++ src/routes/create_transaction.ts
@@ -0,0 +1,42 @@
+import express from "express";
+
+import { Transaction, TransactionType } from "../entities/Transaction";
+import { Client } from "../entities/Client";
+
+const router = express.Router();
+
+router.post("/api/client/:clientId/transaction", async (req, res) => {
+  const { clientId } = req.params;
+
+  const { type, amount } = req.body;
+
+  const client = await Client.findOne(parseInt(clientId));
+
+  if (!client) {
+    return res.json({
+      msg: "client not found",
+    });
+  }
+
+  const transaction = Transaction.create({
+    amount,
+    type,
+    client,
+  });
+
+  await transaction.save();
+
+  if (type === TransactionType.DEPOSIT) {
+    client.balance = client.balance + amount;
+  } else if (type === TransactionType.WITHDRAW) {
+    client.balance = client.balance - amount;
+  }
+
+  await client.save();
+
+  return res.json({
+    msg: "trasaction add",
+  });
+});
+
+export { router as createTransactionRouter };
